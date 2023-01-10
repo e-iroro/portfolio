e9c44d0d73c5d3476d9e78cd13a59d9c22a5ce92
e-iroro <eruiroro18@gmail.com>
2023-01-10 14:47:15 +0100

    add template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Navbar from "./components/Navbar";
-
 function App() {
   return (
-    <div className=" w-5/6 mx-auto">
-      <Navbar />
+    <div className=" w-1/2 mx-auto">
+            <img
+        src="https://res.cloudinary.com/daofgugyr/image/upload/v1673340611/My%20portfoli/Eru_Iroro_Logo.png"
+        alt="Eru Iroro Logo"
+        width={540}
+        className= " mx-auto"
+      />
+      <p className=" text-5xl text-center ">Coming Soon</p>
     </div>
 
   );
